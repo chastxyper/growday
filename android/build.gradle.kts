85e59adc97cb043c485411a58e8719ae9a0c455d
cxyy3 <chastxyper3@gmail.com>
2025-09-23 14:54:36 +0800

    day 1: initialize Firebase in project

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,17 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        // Android Gradle Plugin
+        classpath("com.android.tools.build:gradle:8.0.2")
+
+        // Google Services plugin for Firebase
+        classpath("com.google.gms:google-services:4.4.3")
+    }
+}
+
 allprojects {
     repositories {
         google()
@@ -12,6 +26,7 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
